96034f2b535f99985d7baddaf8723be86ddd2ba4
benhowarth <benhowarthmail@gmail.com>
2018-10-19 04:58:08 +0100

    0.0.2
    Basics of cooking an object

---
+++ Assets/Cookable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cookable : MonoBehaviour {
+	public float cookHp=100.0f;
+	public float burnHp=100.0f;
+	public float beingCookedAmount=0.0f;
+	public bool cooked=false;
+	public bool burnt = false;
+	public Material cookedMat;
+	public Material burntMat;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (beingCookedAmount > 0) {
+			cookHp-=beingCookedAmount;
+		}
+		if (cookHp <= -burnHp) {
+			burnt = true;
+			GetComponent<Renderer>().material=burntMat;
+		}else if (cookHp <= 0) {
+			cooked = true;
+			GetComponent<Renderer>().material=cookedMat;
+		}
+	
+	}
+}
---
+++ Assets/DraggableRB_Old.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class DraggableRB_Old : MonoBehaviour
+{
+	
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	
+	void OnMouseDown(){
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}
+	
+	void OnMouseDrag() {
+		Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+		//transform.position = cursorPosition;
+		gameObject.GetComponent<Rigidbody>().useGravity = false;
+		gameObject.GetComponent<Rigidbody>().isKinematic = true;
+		gameObject.GetComponent<Rigidbody>().MovePosition(cursorPosition);
+		//gameObject.GetComponent<Rigidbody>().velocity = cursorPosition * 5.0f * Time.deltaTime; 
+	}
+	
+	void OnMouseUp(){
+		
+		gameObject.GetComponent<Rigidbody> ().useGravity = true;
+		gameObject.GetComponent<Rigidbody>().isKinematic = false;
+	}
+}
